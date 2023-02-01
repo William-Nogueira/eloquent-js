d2b4937382417f7add8932e507aa0e1c02dafbcc
William Nogueira <wdsn.dev@gmail.com>
2023-02-01 12:00:00 -0700

    fizzbuzz

--- chapter2.js
+++ chapter2.js
@@ -26,3 +26,10 @@ For numbers divisible by 3, print "Fizz" instead of the number, and for numbers 
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 
 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
+
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
+  }
