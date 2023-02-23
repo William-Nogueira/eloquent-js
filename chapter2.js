6ddc331593e00cf5541d20765189cf9872f9bf02
William Nogueira <wdsn.dev@gmail.com>
2023-02-23 12:00:00 -0700

    chapter 2 fix + chapter 4 exercise (reversing an array)

--- chapter2.js
+++ chapter2.js
@@ -27,12 +27,19 @@ When you have that working, modify your program to print "FizzBuzz" for numbers 
 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
 
-for (let n = 1; n <= 100; n++) {
-    let output = "";
-    if (n % 3 == 0) output += "Fizz";
-    if (n % 5 == 0) output += "Buzz";
-    console.log(output || n);
+for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
+
+
 
 // Chessboard
 
--- chapter4.js
+++ chapter4.js
@@ -26,3 +26,35 @@ function range(start, end, step = start < end ? 1 : -1) {
     }
     return total;
   }
+
+// Reversing an array
+
+/* Arrays have a reverse method that changes the array by inverting the order in which its elements appear. 
+For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, 
+takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, 
+does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, 
+which variant do you expect to be useful in more situations? Which one runs faster? */
+
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
