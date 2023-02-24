2239e6d55eabbf27ae34d177c427ac33437eb845
William Nogueira <wdsn.dev@gmail.com>
2023-02-24 12:00:00 -0700

    chapter 4 - arrayToList

--- chapter4.js
+++ chapter4.js
@@ -58,3 +58,39 @@ console.log(reverseArray(["A", "B", "C"]));
 let arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
+
+// A list
+
+/* Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. 
+Also write a listToArray function that produces an array from a list. Then add a helper function prepend, 
+which takes an element and a list and creates a new list that adds the element to the front of the input list, 
+and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) 
+or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth. */
+
+function arrayToList(array) {
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest: list};
+  }
+  return list;
+}
+
+function listToArray(list) {
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}
